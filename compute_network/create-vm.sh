7b49dd0986ae823da2cab3504b17591b6105fdc5
Ganesh <72350742+ganeshch0209@users.noreply.github.com>
2025-07-30 13:54:53 +0530

    Update create-vm.sh

--- compute_network/create-vm.sh
+++ compute_network/create-vm.sh
@@ -1,66 +1,46 @@
 #!/bin/bash
 
-# Create Resource Group
-az group create \
-  --name rg-infra-lab \
-  --location eastus
+# Variables
+location="eastus"
+rg="az104-lab-rg"
+vnet="az104-vnet"
+subnet="az104-subnet"
+nsg="az104-nsg"
+nic="az104-nic"
+vm="az104-vm"
+ip="az104-ip"
+user="azureuser"
+image="UbuntuLTS"
 
-# Create Virtual Network and Subnet
-az network vnet create \
-  --resource-group rg-infra-lab \
-  --name vnet-lab \
+# Create resource group
+az group create --name $rg --location $location
+
+# Create VNet and Subnet
+az network vnet create --resource-group $rg --name $vnet \
   --address-prefix 10.0.0.0/16 \
-  --subnet-name subnet-web \
-  --subnet-prefix 10.0.1.0/24
+  --subnet-name $subnet --subnet-prefix 10.0.1.0/24
 
-# Create Network Security Group
-az network nsg create \
-  --resource-group rg-infra-lab \
-  --name nsg-web
+# Create NSG with SSH rule
+az network nsg create --resource-group $rg --name $nsg
+az network nsg rule create --resource-group $rg --nsg-name $nsg \
+  --name Allow-SSH --priority 1000 --protocol Tcp \
+  --direction Inbound --source-address-prefixes '*' \
+  --source-port-ranges '*' --destination-port-ranges 22 \
+  --access Allow
 
-# Create NSG Rule for SSH
-az network nsg rule create \
-  --resource-group rg-infra-lab \
-  --nsg-name nsg-web \
-  --name allow-ssh \
-  --protocol tcp \
-  --direction inbound \
-  --priority 1000 \
-  --source-address-prefixes '*' \
-  --source-port-ranges '*' \
-  --destination-port-ranges 22 \
-  --access allow
+# Create public IP
+az network public-ip create --resource-group $rg --name $ip
 
-# Create Public IP
-az network public-ip create \
-  --resource-group rg-infra-lab \
-  --name pip-web
+# Create NIC
+az network nic create --resource-group $rg --name $nic \
+  --vnet-name $vnet --subnet $subnet \
+  --network-security-group $nsg \
+  --public-ip-address $ip
 
-# Create NIC and Attach NSG
-az network nic create \
-  --resource-group rg-infra-lab \
-  --name nic-web \
-  --vnet-name vnet-lab \
-  --subnet subnet-web \
-  --network-security-group nsg-web \
-  --public-ip-address pip-web
+# Create VM
+az vm create --resource-group $rg --name $vm \
+  --image $image --admin-username $user \
+  --generate-ssh-keys --nics $nic
 
-# Create Ubuntu VM
-az vm create \
-  --resource-group rg-infra-lab \
-  --name vm-web \
-  --nics nic-web \
-  --image UbuntuLTS \
-  --admin-username azureuser \
-  --generate-ssh-keys
-
-# Output Public IP
-az vm show \
-  --resource-group rg-infra-lab \
-  --name vm-web \
-  --show-details \
-  --query publicIps \
-  --output tsv
-
-# Optional Cleanup
-# az group delete --name rg-infra-lab --yes --no-wait
+# Output IP
+az vm list-ip-addresses --resource-group $rg --name $vm --output table
