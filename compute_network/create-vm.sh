3a250d6fa45764e6b60f54d80364f0fcdd18f0e3
Ganesh <72350742+ganeshch0209@users.noreply.github.com>
2025-07-30 14:15:39 +0530

    Update create-vm.sh

--- compute_network/create-vm.sh
+++ compute_network/create-vm.sh
@@ -10,7 +10,7 @@ nic="az104-nic"
 vm="az104-vm"
 ip="az104-ip"
 user="azureuser"
-image="UbuntuLTS"
+image="ubuntu-24_04-lts"
 
 # Create resource group
 az group create --name $rg --location $location
